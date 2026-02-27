7b46c5ba82aaebda92ad39a6040867c01d9062b9
KazumiMajigami <hugogario990@gmail.com>
2026-02-27 12:43:27 +0700

    Initial commit

---
+++ app/src/main/java/com/jegleg/runningtracker/WelcomeFragment.kt
@@ -0,0 +1,35 @@
+package com.jegleg.runningtracker
+
+import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class WelcomeFragment : Fragment
+    (R.layout.fragment_welcome) {
+
+    private var currentPage = 1
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val page1 = view.findViewById<LinearLayout>(R.id.page1)
+        val page2 = view.findViewById<LinearLayout>(R.id.page2)
+        val fab = view.findViewById<FloatingActionButton>(R.id.fabNext)
+
+        fab.setOnClickListener {
+            if (currentPage == 1) {
+                page1.visibility = View.GONE
+                page2.visibility = View.VISIBLE
+                currentPage = 2
+            } else {
+                parentFragmentManager.beginTransaction()
+                    .replace(R.id.fragmentContainer, LoginFragment())
+                    .addToBackStack(null)
+                    .commit()
+            }
+        }
+        }
+    }
+}
